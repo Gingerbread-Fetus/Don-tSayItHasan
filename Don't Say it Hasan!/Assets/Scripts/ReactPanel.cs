34371cb72caf873ca99456763ecd19dbddb5e1e3
Hayden Matthews <haydenamatthews@yahoo.com>
2021-08-03 20:04:58 -0600

    Combined two scripts and started to implement switching between selected words.

--- Don't Say it Hasan!/Assets/Scripts/ReactPanel.cs
+++ Don't Say it Hasan!/Assets/Scripts/ReactPanel.cs
@@ -2,38 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReactPanel : MonoBehaviour
+namespace CoreInput
 {
-    [SerializeField] int maxWordsOnScreen = 3;
-    [SerializeField] GameObject targetWordPrefab;
-    [SerializeField] float yOffset = 100;
-    [SerializeField] float xOffset = 100;
-
-    private RectTransform m_RectTransform;
-    private TextTarget selected;//TODO: Only the selected should be receiving input.
-
-    void Start()
+    public class ReactPanel : MonoBehaviour
     {
-        m_RectTransform = GetComponent<RectTransform>();
+        [SerializeField] int startingNumberOfWords = 3;
+        [SerializeField] GameObject targetWordPrefab;
+        [SerializeField] float yOffset = 100;
+        [SerializeField] float xOffset = 100;
 
-        for (int i = 0; i < maxWordsOnScreen; i++)
+        private RectTransform m_RectTransform;
+        // private List<TextTarget> targets;
+        private Queue<TextTarget> targets;
+        public static TextTarget selected;
+
+        void Start()
         {
-            AddNewWord();
+            m_RectTransform = GetComponent<RectTransform>();
+
+            for (int i = 0; i < startingNumberOfWords; i++)
+            {
+                AddNewWord();
+            }
+
+            //Set the first TextTarget child of m_Rect to be the selected word.
+            targets = new Queue<TextTarget>(transform.GetComponentsInChildren<TextTarget>());
+            selected = targets.Dequeue();
+            selected.isSelected = true;
         }
 
-        //TODO: Set the first child of m_Rect to be the selected word.
-    }
+        private void Update() 
+        {
+            if(Input.GetButtonDown("Switch"))
+            {
+                SwitchTarget();
+            }
+        }
 
-    public void AddNewWord()
-    {
-        GameObject newWord = Instantiate(targetWordPrefab, transform, false);
-        MoveWord(newWord.GetComponent<RectTransform>());
-    }
+        private void SwitchTarget()
+        {
+            selected.isSelected = false;
+            targets.Enqueue(selected);
+            selected = targets.Dequeue();
+            selected.isSelected = false;
+        }
 
-    public void MoveWord(RectTransform word)
-    {
-        float randX = Random.Range(m_RectTransform.rect.xMin + xOffset, m_RectTransform.rect.xMax - xOffset);
-        float randY = Random.Range(m_RectTransform.rect.yMin + yOffset, m_RectTransform.rect.yMax - yOffset);
-        word.localPosition = new Vector3(randX, randY, 0);
+        public void AddNewWord()
+        {
+            GameObject newWord = Instantiate(targetWordPrefab, transform, false);
+            MoveWord(newWord.GetComponent<RectTransform>());
+        }
+
+        public void MoveWord(RectTransform word)
+        {
+            float randX = Random.Range(m_RectTransform.rect.xMin + xOffset, m_RectTransform.rect.xMax - xOffset);
+            float randY = Random.Range(m_RectTransform.rect.yMin + yOffset, m_RectTransform.rect.yMax - yOffset);
+            word.localPosition = new Vector3(randX, randY, 0);
+        }
     }
 }
+
--- Don't Say it Hasan!/Assets/Scripts/TextTarget.cs
+++ Don't Say it Hasan!/Assets/Scripts/TextTarget.cs
@@ -1,31 +1,134 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class TextTarget : MonoBehaviour
+namespace CoreInput
 {
-    private TextMeshProUGUI gt;
-    private RectTransform m_RectTransform;
-    WordQueue queue;
-    void Awake() 
+    public class TextTarget : MonoBehaviour
     {
-        queue = GameObject.FindGameObjectWithTag("Word Queue").GetComponent<WordQueue>();
-        m_RectTransform = GetComponent<RectTransform>();
-    }
+        [SerializeField] Color32 myColor;
+        Color32[] newVertexColors;
+        private TMP_Text m_TextComponent;
+        TMP_TextInfo textInfo;
+        TextTarget textTarget;
+        Color32 c0;
+        private RectTransform m_RectTransform;
+        string currentString = "";
+        int currentCharacter = 0;
+        WordQueue queue;
 
-    void Start()
-    {
-        gt = GetComponent<TextMeshProUGUI>();
-        gt.text = queue.GetNextWord();
-        GetComponent<KBInListener>().targetString = gt.text;
-    }
+        [HideInInspector]public bool isSelected = false;
+        [HideInInspector]public string targetString;
+        void Awake() 
+        {
+            queue = GameObject.FindGameObjectWithTag("Word Queue").GetComponent<WordQueue>();
+            m_RectTransform = GetComponent<RectTransform>();
+            m_TextComponent = GetComponent<TMP_Text>();
+            textInfo = m_TextComponent.textInfo;
+            m_TextComponent.text = queue.GetNextWord();
+            targetString = m_TextComponent.text;
+        }
 
-    public void ChangeWord()
-    {
-        gt.text = queue.GetNextWord();
-        GetComponent<KBInListener>().targetString = gt.text;
-        //TODO: Play effect
-        GetComponentInParent<ReactPanel>().MoveWord(m_RectTransform);
+        void Update()
+        {
+            HandleInput();
+        }
+        private void HandleInput()
+        {
+            if (isSelected)
+            {
+                foreach (char c in Input.inputString)
+                {
+                    if (c == '\b') // has backspace/delete been pressed?
+                    {
+                        currentString = "";
+                        currentCharacter = 0;
+                    }
+                    else if ((c == '\n') || (c == '\r')) // enter/return
+                    {
+                        //check against final text, mostly as a failsafe
+                        if (targetString == currentString) print("correct");
+                        else print("WRONG DUD");
+                        currentString = "";
+                        currentCharacter = 0;
+                    }
+                    else
+                    {
+                        currentString += c;
+                        currentCharacter = currentString.Length;
+                        if (currentString == targetString.Substring(0, currentString.Length))
+                        {
+                            MarkCorrectCharacters();
+                        }
+                        else
+                        {
+                            m_TextComponent.ForceMeshUpdate();//I think this resets the mesh.
+                            currentString = "";
+                            print("WRONG DUD");
+                            currentCharacter = 0;
+                        }
+
+                        if (targetString == currentString)
+                        {
+                            m_TextComponent.ForceMeshUpdate();
+                            ChangeWord();
+                            print("correct");
+                            currentString = "";
+                            currentCharacter = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void ChangeWord()
+        {
+            m_TextComponent.text = queue.GetNextWord();
+            targetString = m_TextComponent.text;
+            GetComponentInParent<ReactPanel>().MoveWord(m_RectTransform);
+        }
+        public void SetText(string newText)
+        {
+            m_TextComponent.text = newText;
+            targetString = m_TextComponent.text;
+        }
+        private void MarkCorrectCharacters()
+        {
+            m_TextComponent.ForceMeshUpdate();
+
+            TMP_TextInfo textInfo = m_TextComponent.textInfo;
+
+            Color32[] newVertexColors;
+            Color32 c0 = m_TextComponent.color;
+
+            for (int i = 0; i < currentCharacter; i++)
+            {
+                int characterCount = textInfo.characterCount;
+                if (characterCount == 0) return;
+                // Get the index of the material used by the current character.
+                int materialIndex = textInfo.characterInfo[i].materialReferenceIndex;
+                // Get the vertex colors of the mesh used by this text element (character or sprite).
+                newVertexColors = textInfo.meshInfo[materialIndex].colors32;
+                // Get the index of the first vertex used by this text element.
+                int vertexIndex = textInfo.characterInfo[i].vertexIndex;
+                // Only change the vertex color if the text element is visible.
+                if (textInfo.characterInfo[i].isVisible)
+                {
+                    c0 = new Color32((byte)199, (byte)55, (byte)55, 255);
+                    // c0 = myColor;
+
+                    newVertexColors[vertexIndex + 0] = c0;
+                    newVertexColors[vertexIndex + 1] = c0;
+                    newVertexColors[vertexIndex + 2] = c0;
+                    newVertexColors[vertexIndex + 3] = c0;
+
+                    // New function which pushes (all) updated vertex data to the appropriate meshes when using either the Mesh Renderer or CanvasRenderer.
+                    m_TextComponent.UpdateVertexData(TMP_VertexDataUpdateFlags.Colors32);
+
+                    // This last process could be done to only update the vertex data that has changed as opposed to all of the vertex data but it would require extra steps and knowing what type of renderer is used.
+                    // These extra steps would be a performance optimization but it is unlikely that such optimization will be necessary.
+                }
+            }
+        }
     }
 }
