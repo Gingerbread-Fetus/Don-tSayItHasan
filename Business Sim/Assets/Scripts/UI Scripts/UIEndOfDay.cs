b182f4a4f13359f2c92a973d5af6deb664dfe016
Hayden Matthews <haydenamatthews@gmail.com>
2021-10-13 17:07:38 -0600

    Added the functionality to switch between pictures.

--- Business Sim/Assets/Scripts/UI Scripts/UIEndOfDay.cs
+++ Business Sim/Assets/Scripts/UI Scripts/UIEndOfDay.cs
@@ -9,11 +9,21 @@ namespace UI
     public class UIEndOfDay : MonoBehaviour
     {
         [SerializeField] Image UIImage;
+        List<Sprite> girlImages = new List<Sprite>();
+        int currentImage = 0;
+        private void Awake() {
+            if(gameObject.activeInHierarchy) gameObject.SetActive(false);
+        }
+        
         private void OnEnable() {
             UIImage.sprite = girlImages[0];
+            currentImage = 0;
         }
 
-        List<Sprite> girlImages = new List<Sprite>();
+        private void OnDisable() {
+            girlImages.Clear();
+        }
+
         public void SetPictures(SlaveGirl[] girls)
         {
             foreach (SlaveGirl girl in girls)
@@ -22,6 +32,22 @@ namespace UI
                 int index = Random.Range(0, girl.images.Length);
                 girlImages.Add(girl.images[index]);
             }
+        }
+
+        public void NextImage()
+        {
+            print("Next Image");
+            currentImage = (currentImage + 1 > (girlImages.Count - 1)) ? 0 : currentImage + 1;
+            UIImage.sprite = girlImages[currentImage];
+            print(currentImage);
+        }
+
+        public void PrevImage()
+        {
+            print("Previous Image");
+            currentImage = (currentImage - 1 < 0) ? girlImages.Count - 1 : currentImage - 1;
+            UIImage.sprite = girlImages[currentImage];
+            print(currentImage);
         }
     }
 }
